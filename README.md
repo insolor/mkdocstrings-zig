de1318b041c78cd660fc82cf892eb34d09f8f5e7
insolor <2442833+insolor@users.noreply.github.com>
2025-07-19 14:47:33 +0300

    Init project, add minimal zig parser

---
+++ README.md
@@ -0,0 +1,16 @@
+# mkdocstrings-zig
+
+[![ci](https://github.com/insolor/mkdocstrings-zig/workflows/ci/badge.svg)](https://github.com/insolor/mkdocstrings-zig/actions?query=workflow%3Aci)
+[![documentation](https://img.shields.io/badge/docs-mkdocs-708FCC.svg?style=flat)](https://insolor.github.io/mkdocstrings-zig/)
+[![pypi version](https://img.shields.io/pypi/v/mkdocstrings-zig.svg)](https://pypi.org/project/mkdocstrings-zig/)
+[![gitter](https://badges.gitter.im/join%20chat.svg)](https://app.gitter.im/#/room/#mkdocstrings-zig:gitter.im)
+
+A Zig handler for mkdocstrings.
+
+## Installation
+
+```bash
+pip install mkdocstrings-zig
+```
+
+
---
+++ tests/test_zig_parser_minimal.py
@@ -0,0 +1,30 @@
+from mkdocstrings_handlers.zig._internal.zig_parser_minimal import _parse_zig_code
+
+
+def test_parser():
+    zig_code = """
+    //! This is module-level documentation
+    //! It describes the entire file
+
+    /// Adds two numbers.
+    fn add(a: i32, b: i32) i32 {
+        return a + b;
+    }
+
+    /// A constant named PI.
+    const PI = 3.14159;
+
+    /// A 2D point struct.
+    const Point = struct {
+        x: i32,
+        y: i32,
+    };
+    """
+
+    parsed = _parse_zig_code(zig_code)
+    assert parsed == {
+        "functions": [{"name": "add", "doc": "Adds two numbers."}],
+        "constants": [{"name": "PI", "doc": "A constant named PI."}],
+        "module_docs": "This is module-level documentation\nIt describes the entire file",
+        "structs": [{"name": "Point", "doc": "A 2D point struct."}],
+    }
