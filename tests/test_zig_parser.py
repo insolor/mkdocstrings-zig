bbee7c1c456bac201990da2c00ab2894fbc0dee2
insolor <2442833+insolor@users.noreply.github.com>
2025-07-20 22:23:22 +0300

    Fix test_zig_parser.py, add struct support

--- tests/test_zig_parser.py
+++ tests/test_zig_parser.py
@@ -28,11 +28,24 @@ def test_parser():
 
     parsed = ZigHandler._parse_zig_code(zig_code)
     assert parsed == {
-        "module_docs": ["This is module-level documentation", "It describes the entire file"],
+        "module_docs": [
+            "This is module-level documentation",
+            "It describes the entire file",
+        ],
         "functions": [
-            {"name": "add", "doc": "Adds two numbers.", "signature": "fn add(a: i32, b: i32) i32"},
+            {
+                "name": "add",
+                "doc": "Adds two numbers.",
+                "signature": "fn add(a: i32, b: i32) i32",
+            },
             {"name": "main", "doc": "Main function", "signature": "pub fn main() void"},
         ],
-        "constants": [{"name": "PI", "doc": "A constant named PI."}],
+        "constants": [
+            {
+                "name": "PI",
+                "signature": "const PI = 3.14159;",
+                "doc": "A constant named PI.",
+            }
+        ],
         "structs": [{"name": "Point", "doc": "A 2D point struct."}],
     }
